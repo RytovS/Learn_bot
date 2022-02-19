bae6bc52e98977d1b3bbeccb008bbf6c1197e5ce
Sergey Rytov <komatozz89@yandex.ru>
2022-02-19 22:55:57 +0300

    добавил данные о пользователе

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import logging
+
 from os import set_inheritable
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -11,12 +12,14 @@ PROXY = {'proxy_url': settings.PROXY_URL ,
 
 def greet_user(update, context):
     print ('Вызван /start')
-    update.message.reply_text('Здравствуй, человек Х')
 
+     
 def talk_to_me(update, context):
     text = update.message.text
     print (text)
     update.message.reply_text(text)
+    user = update.message.from_user
+    print('Ты разговариваешь с {} and его ID: {} '.format(user['username'], user['id']))
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
@@ -24,6 +27,7 @@ def main():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    
 
 
     logging.info("Бот стартовал")
